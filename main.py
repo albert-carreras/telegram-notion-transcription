1f82ce07bd3781bec412f14b1780380bc253358c
albert <alb.carreras@gmail.com>
2024-08-02 23:39:52 +0200

    Telegram -> notion journaling

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import os
-import threading
+import sys
+from datetime import datetime
 from contextlib import contextmanager
 
-import numpy as np
-import ollama
-import sounddevice as sd
 import whisperx
-from scipy.io.wavfile import write
-from styletts2 import tts
+from openai import OpenAI
+from telegram import Update
+from telegram.ext import Application, ContextTypes, CommandHandler, MessageHandler, filters
+from notion_client import Client
+from notion_client.errors import APIResponseError
 
-sample_rate = 24000
-channels = 1
-device = "cpu"
-audio_file = "input.wav"
-batch_size = 1
-compute_type = "int8"
+# Constants
+SAMPLE_RATE = 24000
+CHANNELS = 1
+DEVICE = "cpu"
+AUDIO_FILE = "input.wav"
+BATCH_SIZE = 4
+COMPUTE_TYPE = "int8"
 
-
+client = OpenAI(
+    api_key=OPENAI_API_KEY,
+)
 @contextmanager
 def suppress_stdout_stderr():
     with open(os.devnull, "w") as devnull:
@@ -31,128 +35,174 @@ def suppress_stdout_stderr():
             sys.stderr = old_stderr
 
 
+print("Loading WhisperX model...")
 with suppress_stdout_stderr():
-    whisper_model = whisperx.load_model(
-        "base", device, compute_type=compute_type, language="en"
-    )
+    whisper_model = whisperx.load_model("large-v3", DEVICE, compute_type=COMPUTE_TYPE, language="en", threads=8)
 
-global styletts
-styletts = None
+notion = Client(auth=NOTION_TOKEN)
 
 
-def load_styletts():
-    global styletts
+def transcribe_audio(audio_file):
+    audio = whisperx.load_audio(audio_file)
+    print("--- Transcribing ---")
     with suppress_stdout_stderr():
-        styletts = tts.StyleTTS2()
+        result = whisper_model.transcribe(audio, batch_size=BATCH_SIZE)
+
+    if result["segments"]:
+        return "\n".join(segment["text"] for segment in result["segments"])
+    else:
+        return "No speech detected."
 
 
-class ConversationManager:
-    def __init__(self):
-        self.history = [
-            {
-                "role": "system",
-                "content": """You are a friendly person that answers in short sentences.
-        You are having a casual conversation and you're interested in the person talking to you.""",
-            }
+def save_to_notion(transcription, raw):
+    now = datetime.now()
+
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "You are an assistant that generates concise, meaningful titles for journal entries based on their content."},
+            {"role": "user", "content": transcription}
         ]
+    )
+    title = response.choices[0].message.content.strip('\"')
 
-    def add_user_message(self, message):
-        self.history.append({"role": "user", "content": message})
+    response_title = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "You are an assistant that summarizes journal entries in a couple of sentences maximum. The summary is addressed to them, the user."},
+            {"role": "user", "content": transcription}
+        ]
+    )
+    summary = response_title.choices[0].message.content
 
-    def add_assistant_message(self, message):
-        self.history.append({"role": "assistant", "content": message})
-
-    def get_history(self):
-        return self.history
-
-
-def record_audio():
-    recording = []
-
-    def callback(indata, frames, time, status):
-        if status:
-            print("Recording Error:", status)
-        recording.append(indata.copy())
-
-    print("\n--- Recording started. Press Enter to stop recording. ---")
     try:
-        with sd.InputStream(
-            samplerate=sample_rate, channels=channels, callback=callback
-        ):
-            input()
-    except Exception as e:
-        print("An error occurred during recording:", e)
-
-    return np.concatenate(recording) if recording else np.zeros((sample_rate, channels))
-
-
-def play_wav_array(wav_array):
-    sd.play(wav_array, samplerate=sample_rate, blocking=True)
-    sd.wait()
-
-
-def generate_response(conversation_manager):
-    try:
-        response = ollama.chat(
-            model="llama3",
-            messages=conversation_manager.get_history(),
+        new_page = notion.pages.create(
+            parent={"database_id": NOTION_JOURNAL_PAGE_ID},
+            properties={
+                "Title": {"type": "title",
+                          "title": [{"type": "text", "text": {"content": title}}]},
+                "Date": {
+                    "type": "date",
+                    "date": {"start": now.isoformat(), "end": None}}
+            },
+            children=[
+                {
+                    "object": "block",
+                    "type": "paragraph",
+                    "paragraph": {
+                        "rich_text": [{"type": "text", "text": {"content": f"\n"}}]
+                    }
+                },
+                {
+                    "object": "block",
+                    "type": "quote",
+                    "quote": {
+                        "rich_text": [{
+                            "annotations": {
+                                "italic": True,
+                            },
+                            "type": "text",
+                            "text": {"content": f"{summary}"}
+                        }]
+                    }
+                },
+                {
+                    "object": "block",
+                    "type": "paragraph",
+                    "paragraph": {
+                        "rich_text": [{"type": "text", "text": {"content": f"\n\n{transcription}\n\n"}}]
+                    }
+                },
+                {
+                    "object": "block",
+                    "type": "toggle",
+                    "toggle": {
+                        "rich_text": [{"type": "text", "text": {"content": "Raw Data"}}],
+                        "children": [
+                            {
+                                "object": "block",
+                                "type": "paragraph",
+                                "paragraph": {
+                                    "rich_text": [{"type": "text", "text": {"content": raw}}]
+                                }
+                            }
+                        ]
+                    }
+                }
+            ]
         )
 
-        assistant_response = response["message"]["content"]
-        conversation_manager.add_assistant_message(assistant_response)
+        print(f"Journal entry saved to Notion page: {new_page['url']}")
+        return True, new_page['url']
 
-        print("--- TTS Generating ---")
-        if styletts is None:
-            print("StyleTTS not ready yet.")
+    except APIResponseError as e:
+        error_message = f"Error saving journal entry to Notion: {str(e)}"
+        print(error_message)
+        return False, error_message
+
+
+async def handle_voice_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message = update.message
+    await update.message.reply_text("Transcribing...")
+
+    if message.voice:
+        try:
+            file = await context.bot.get_file(message.voice.file_id)
+            await file.download_to_drive(AUDIO_FILE)
+        except Exception as e:
+            await update.message.reply_text(f"Error downloading file: {e}")
             return
-        with suppress_stdout_stderr():
-            wav = styletts.inference(assistant_response, diffusion_steps=4)
-        print("--- Response Playback ---")
-        play_wav_array(wav)
-    except Exception as e:
-        print(f"An error occurred during response generation: {str(e)}")
+
+        try:
+            transcription = transcribe_audio(AUDIO_FILE)
+        except Exception as e:
+            await update.message.reply_text(f"Error transcribing audio: {e}")
+            return
+
+        try:
+            raw = transcription
+            transcription = cleanup_with_gpt4o_mini(transcription)
+        except Exception as e:
+            await update.message.reply_text(f"Error cleaning up text with GPT-4o mini: {e}")
+            return
+
+        success, url = save_to_notion(transcription, raw)
+
+        if success:
+            await update.message.reply_text(f"Saved: {url}")
+        else:
+            await update.message.reply_text("Error saving to Notion. Please check your settings.")
+    else:
+        await update.message.reply_text("No voice message detected.")
+
+
+def cleanup_with_gpt4o_mini(text):
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system",
+             "content": "You are an assistant that proofreads and cleans up text. You may correct grammar, spelling, and punctuation, but you will not change the meaning of the text. Only respond with the cleaned-up text."},
+            {"role": "user", "content": text}
+        ]
+    )
+    cleaned_text = response.choices[0].message.content
+    return cleaned_text
+
+
+async def start(update, context):
+    await update.message.reply_text(
+        "Welcome! Send me a voice message and I'll transcribe it and save it to your Notion journal.")
 
 
 def main():
-    conversation_manager = ConversationManager()
-    styletts_thread = threading.Thread(target=load_styletts)
-    styletts_thread.start()
 
-    while True:
-        try:
-            recording = record_audio()
+    application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
 
-            if not np.any(recording):
-                print("No audio recorded, please try again.")
-                continue
-            write(audio_file, sample_rate, recording)
-            audio = whisperx.load_audio(audio_file)
-            print("--- WhisperX Transcribing ---")
-            if whisper_model is None:
-                print("WhisperX not ready yet, please wait...")
-                return
-            with suppress_stdout_stderr():
-                result = whisper_model.transcribe(audio, batch_size=batch_size)
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(MessageHandler(filters.VOICE, handle_voice_message))
 
-            user_input = (
-                result["segments"][0]["text"]
-                if result["segments"]
-                else "No speech detected."
-            )
-            conversation_manager.add_user_message(user_input)
-
-            print("--- LLM Generating ---")
-            generate_response(conversation_manager)
-
-            print(
-                "\n--- Press Enter to continue the conversation or type 'q' to exit ---"
-            )
-            user_choice = input().strip().lower()
-            if user_choice == "q":
-                print("Exiting conversation")
-                break
-        except Exception as e:
-            print(f"An error occurred: {str(e)}")
+    print("Bot is running. Send a voice message to transcribe and save to Notion.")
+    application.run_polling()
 
 
 if __name__ == "__main__":
